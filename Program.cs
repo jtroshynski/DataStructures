de5812ad65747f41ee5099e8346ee611148b01f5
Jeremy Troshynski <jtroshynski@gmail.com>
2019-10-07 15:00:24 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -8,8 +8,17 @@ namespace DataStructuresProject
         public static void Main(string[] args)
         {
             Queue<Node> queue = new Queue<Node>();
-            Node startNode = new Node(1,new Node(2,new Node(4,null,new Node(7))),
-                new Node(3,new Node(5,null,new Node(8,new Node(9),new Node(10))),new Node(6)));
+            Node startNode = 
+                new Node(1,
+                    new Node(2,
+                        new Node(4,null,
+                            new Node(7))),
+                    new Node(3,
+                        new Node(5,null,
+                            new Node(8,
+                                new Node(9),
+                                new Node(10))),
+                        new Node(6)));
 
             /*
              *        1
