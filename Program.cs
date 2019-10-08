854ec38264796fcc384ac26b776527e176019bac
Jeremy Troshynski <jtroshynski@gmail.com>
2019-10-08 11:27:34 -0500

    Made the BST a true BST with correct values

--- Program.cs
+++ Program.cs
@@ -8,27 +8,30 @@ namespace DataStructuresProject
         public static void Main(string[] args)
         {
             Node startNode = 
-                new Node(1,
-                    new Node(2,
-                        new Node(4,null,
-                            new Node(7))),
+                new Node(5,
                     new Node(3,
-                        new Node(5,null,
+                        new Node(1,
+                            new Node(0),
+                            null),
+                        new Node(4)),
+                    new Node(10,
+                        new Node(6,
+                            null,
                             new Node(8,
-                                new Node(9),
-                                new Node(10))),
-                        new Node(6)));
+                                new Node(7),
+                                new Node(9))),
+                        new Node(11)));
 
             /*
-             *        1
-             *      /   \
-             *     2     3
-             *    /    /  \
-             *   4    5    6
-             *    \    \  
-             *     7    8 
-             *         / \
-             *        9   10
+             *          5
+             *        /   \
+             *       3     10
+             *      / \   /  \
+             *     1   4 6    11
+             *   /        \  
+             *  0          8 
+             *            / \
+             *           7   9
              */
 
             var sorter = new Sorter();
